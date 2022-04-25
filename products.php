d7c5510960a092fee6f9d8e986c4454e2ad9bbf0
natasakis <natkis023@hotmail.com>
2022-04-25 20:42:35 +0200

    Dodat domaci

---
+++ products.php
@@ -0,0 +1,162 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="theme/css/products.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <nav>
+        <ul>
+            <li><a href="#">Online prodavnica</a></li>
+            <li><a href="#">Promocije</a></li>
+            <li><a href="#">Aktuelno</a></li>
+            <li><a href="#">Moja korpa</a></li>
+        </ul>
+        </nav>
+        <a href="#"><img src="theme/img/menu.png" alt="menu" id="menu"></a>
+        <a href="#"><img src="theme/img/filter.png" alt="filter" id="filter"></a>
+    </header>
+    <div class="container">
+        <aside>
+            
+            <div class="aside_checkbox">
+                <label>Brend</label>
+                <ul>
+                    <li><label><input type="checkbox">Grand</label></li>
+                    <li><label><input type="checkbox">Doncafe</label></li>
+                    <li><label><input type="checkbox">Nescafe</label></li>
+                    <li><label><input type="checkbox">C kafa</label></li>
+                    <li><label><input type="checkbox">Bonito</label></li>
+                </ul>
+            </div>
+            <div class="aside_checkbox">
+                <label>Cena</label>
+                <ul>
+                    <li><label><input type="checkbox">0-99,99</label></li>
+                    <li><label><input type="checkbox">99,99-199,99</label></li>
+                    <li><label><input type="checkbox">199,99-299,99</label></li>
+                    <li><label><input type="checkbox">299,99-399,99</label></li>
+                    <li><label><input type="checkbox">399,99+</label></li>
+                </ul>
+            </div>
+            <div class="aside_checkbox">
+                <label>Ocena</label>
+                <ul>
+                    <li><label class="short-label"><input type="checkbox">0-1</label></li>
+                    <li><label class="short-label"><input type="checkbox">1-2</label></li>
+                    <li><label class="short-label"><input type="checkbox">2-3</label></li>
+                    <li><label class="short-label"><input type="checkbox">3-4</label></li>
+                    <li><label class="short-label"><input type="checkbox">4-5</label></li>
+                </ul>
+            </div>
+        </aside>
+        <main>
+            <div class="product">
+                <h6>Grand Gold</h6>
+                <div class="product_subdiv">
+                    <img src="theme/img/grandgold.jpg" alt="Slika kafe">
+                    <div class="product_subsubdiv">
+                        <p class="snizenje">299,99</p>
+                        <p>289,99</p>
+                        <button type="submit">Dodaj!</button>
+                    </div>
+                </div>
+            </div>
+            <div class="product">
+                <h6>Bonito Kafa</h6>
+                <div class="product_subdiv">
+                    <img src="theme/img/bonito.jpg" alt="Slika kafe">
+                    <div class="product_subsubdiv">
+                        <p>229,69</p>
+                        <button type="submit">Dodaj!</button>
+                    </div>
+                </div>
+            </div>
+            <div class="product">
+                <h6>Dolce Gusto</h6>
+                <div class="product_subdiv">
+                    <img src="theme/img/dolcegusto.jpg" alt="Slika kafe">
+                    <div class="product_subsubdiv">
+                        <p>1299,99</p>
+                        <button type="submit">Dodaj!</button>
+                    </div>
+                </div>
+            </div>
+            <div class="product">
+                <h6>Doncafe</h6>
+                <div class="product_subdiv">
+                    <img src="theme/img/doncafe.jpg" alt="Slika kafe">
+                    <div class="product_subsubdiv">
+                        <p>198,89</p>
+                        <button type="submit">Dodaj!</button>
+                    </div>
+                </div>
+            </div>
+            <div class="product">
+                <h6>Doncafe Strong</h6>
+                <div class="product_subdiv">
+                    <img src="theme/img/doncafestrong.jpg" alt="Slika kafe">
+                    <div class="product_subsubdiv">
+                        <p>359,97</p>
+                        <button type="submit">Dodaj!</button>
+                    </div>
+                </div>
+            </div>
+            <div class="product">
+                <h6>Jacobs Crema</h6>
+                <div class="product_subdiv">
+                    <img src="theme/img/jacobscrema.jpg" alt="Slika kafe">
+                    <div class="product_subsubdiv">
+                        <p class="snizenje">789,50</p>
+                        <p>789,49</p>
+                        <button type="submit">Dodaj!</button>
+                    </div>
+                </div>
+            </div>
+            <div class="product">
+                <h6>Lavazza</h6>
+                <div class="product_subdiv">
+                    <img src="theme/img/lavazza.jpg" alt="Slika kafe">
+                    <div class="product_subsubdiv">
+                        <p>1129,99</p>
+                        <button type="submit">Dodaj!</button>
+                    </div>
+                </div>
+            </div>
+            <div class="product">
+                <h6>Nescafe</h6>
+                <div class="product_subdiv">
+                    <img src="theme/img/nescafe.jpg" alt="Slika kafe">
+                    <div class="product_subsubdiv">
+                        <p>699,99</p>
+                        <button type="submit">Dodaj!</button>
+                    </div>
+                </div>
+            </div>
+            <div class="product">
+                <h6>Grand Kafa</h6>
+                <div class="product_subdiv">
+                    <img src="theme/img/grandgold.jpg" alt="Slika kafe">
+                    <div class="product_subsubdiv">
+                        <p>299,99</p>
+                        <button type="submit">Dodaj!</button>
+                    </div>
+                </div>
+            </div>
+        </main>
+    </div>
+    <footer>
+        <nav>
+            <ul>
+                <li><a href="#">Uslovi kupovine</a></li>
+                <li><a href="#">Isporuka</a></li>
+                <li><a href="#">Politika povracaja</a></li>
+            </ul>
+            </nav>
+    </footer>
+    <a href="#"><img id="up_a" src="theme/img/up_arrow.png" alt="up"></a>
+</body>
+</html>
